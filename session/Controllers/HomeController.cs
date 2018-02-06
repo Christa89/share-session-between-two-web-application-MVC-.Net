588cfeabcef8081148d5f049ff6a2e36f2bd680f
Christa89 <christa.bsc@gmail.com>
2018-02-06 15:39:56 +0530

    commited

---
+++ session/Controllers/HomeController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace session.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+           
+            return View();
+        }
+
+
+        public ActionResult Index1()
+        {
+            ViewBag.userId = Session["UserID"].ToString();
+            ViewBag.userName = Session["UserName"].ToString();
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+            string a = Session["UserID"].ToString();
+            string b = Session["UserName"].ToString();
+
+            return new RedirectResult("http://localhost:58225/Home/About");
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+            string a = Session["UserID"].ToString();
+            string b = Session["UserName"].ToString();
+
+            return new RedirectResult("http://localhost:58166/Home/index");
+        }
+    }
+}
---
+++ session1/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(session1.Startup))]
+namespace session1
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
